2a39a4a27ad768ec552c5c5e2c3398dfc11219be
Rubean <rubeansite@gmail.com>
2018-04-12 22:59:02 -0300

    Banco H2, Repositorio e Service

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -3,26 +3,38 @@ package com.example.demo.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
+	@Autowired
+	private CategoriaService service;
 
-	
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria cat = service.buscar(id);
+
+		return ResponseEntity.ok().body(cat);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
 		Categoria c1 = new Categoria(1, "Informática");
 		Categoria c2 = new Categoria(2, "Escritório");
-		
+
 		List<Categoria> lista = new ArrayList<Categoria>();
 		lista.add(c1);
 		lista.add(c2);
-		
+
 		return lista;
 	}
 }
---
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -0,0 +1,27 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	public CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> result = repo.findById(id);
+		return result.orElse(null);
+	}
+	
+	public List<Categoria> listar(){
+		return repo.findAll();
+	}
+
+}

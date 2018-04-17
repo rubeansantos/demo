570ff2bc5a8563952b02c0548098e188e53a1e3a
rubeansantos <rubeansite@gmail.com>
2018-04-16 21:38:29 -0300

    Estrutura de pagamento

--- src/main/java/com/example/demo/domain/PagamentoComBoleto.java
+++ src/main/java/com/example/demo/domain/PagamentoComBoleto.java
@@ -19,7 +19,7 @@ public class PagamentoComBoleto extends Pagamento {
 	 */
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
-	
+
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataPagamento;
 
--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -1,6 +1,5 @@
 package com.example.demo.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +15,18 @@ public class CategoriaService {
 	@Autowired
 	public CategoriaRepository repo;
 
+	/*************************************************************************/
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> result = repo.findById(id);
 		return result.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
-	public List<Categoria> listar() {
-		return repo.findAll();
+	/*************************************************************************/
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
+	/*************************************************************************/
 
 }
